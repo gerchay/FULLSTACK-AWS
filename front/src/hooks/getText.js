ad7ff619c2a2809b065d988ae8e2d7ed2998cb17
Gerardo cHAY <2995058550101@ingenieria.usac.edu.gt>
2021-04-09 11:17:22 -0600

    Cambio de url en hook fron

--- front/src/hooks/getText.js
+++ front/src/hooks/getText.js
@@ -1,7 +1,7 @@
 
 export const getTexts = async( category ) => {
 
-    const url = `http://localhost:5000/iecho?text=${ encodeURI( category ) }`
+    const url = `http://18.223.115.36:5000/iecho?text=${ encodeURI( category ) }`
     const resp = await fetch( url )
     const data  = await resp.json()
     return data
